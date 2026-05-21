60e06f91027fa3e302861f75d3b6c1fa5ffe08c2
Antua <suleykin2005@mail.ru>
2026-05-21 14:51:14 +0300

    обязательный фикс базы

--- app/src/main/java/com/example/fitnessapp/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/fitnessapp/data/remote/RetrofitClient.kt
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
 
 
 object RetrofitClient {
-    private const val BASE_URL = "https://dummyjson.com/"
+    private const val BASE_URL = "http://10.0.2.2:8080"
 
     @Volatile
     private var tokenManager: TokenManager? = null
